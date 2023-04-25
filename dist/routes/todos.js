9941b61ab7c52954551d086ec8d5fc809c475125
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-04-25 13:12:23 +0530

    task 26

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -9,6 +9,8 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    //const body = req.body as {text: string} // before type alias
+    const body = req.body; // after using type alias
     const newTodo = {
         id: new Date().toISOString(),
         text: req.body.text
@@ -17,11 +19,14 @@ router.post('/todo', (req, res, next) => {
     res.status(200).json({ todo: newTodo });
 });
 router.delete('/deletetodo/:todoId', (req, res, next) => {
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    const params = req.params;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId);
     res.status(200).json({ message: 'deleted succesfully', todos: todos });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
     if (todoIndex >= 0) {
         todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -8,12 +8,17 @@ let todos: Todo[] = []; // we used let here so that we can overwrite todos means
 const router = Router()           // can also write like this 
 
 
+type RequestBody = {text: string}
+type RequestParams = {todoId: string}
+
 router.get('/', (req,res, next) =>{
 res.status(200).json({todos: todos })
 })
 
 
 router.post('/todo',(req, res, next)=>{
+  //const body = req.body as {text: string} // before type alias
+const body = req.body as RequestBody // after using type alias
 const newTodo: Todo = {
     id: new Date().toISOString(),
     text: req.body.text
@@ -23,13 +28,17 @@ res.status(200).json({todo: newTodo})
 })
 
 router.delete('/deletetodo/:todoId', (req, res, next)=>{
- todos = todos.filter(todoItem => todoItem.id !== req.params.todoId)
+  const params = req.params as RequestParams
+
+ todos = todos.filter(todoItem => todoItem.id !== params.todoId)
  res.status(200).json({message: 'deleted succesfully', todos: todos})
 })
 
 
 router.put('/todo/:todoId', (req, res, next)=>{
-    const tid = req.params.todoId
+    const params = req.params as RequestParams
+    const tid = params.todoId
+    const body = req.body as RequestBody
     const todoIndex = todos.findIndex(todoItem => todoItem.id === tid)
     if(todoIndex >= 0){
       todos[todoIndex] = {id: todos[todoIndex].id , text: req.body.text};
